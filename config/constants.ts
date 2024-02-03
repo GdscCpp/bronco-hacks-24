ea18cbcf6639b828c7d08490b72e7b32a9b732b8
blbergo <bryan.l.bergo@gmail.com>
2024-02-03 14:38:12 -0800

    build fix - ol by daniel

--- config/constants.ts
+++ config/constants.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = process.env.VERCEL_URL?.slice(0,-1) || "http://localhost:3000";
+export const BASE_URL = process.env.VERCEL_URL?.substring(0, process.env.VERCEL_URL.length) || "http://localhost:3000";
